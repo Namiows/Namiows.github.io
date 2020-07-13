15f972f20b3f53ca5f7dc35fe007d92537de2c17
Kesley Lahoz <namielahoz@gmail.com>
2020-07-13 17:29:06 -0300

    Enviando exercicio 1 para o remoto

---
+++ exercises/4_1/exercicio1.js
@@ -0,0 +1,28 @@
+//Soma de dois valores
+//let a = 35;
+//let b = 40;
+
+//console.log(a+b);
+
+
+//Subtração de dois valores
+
+//let a = 40;
+//let b = 35;
+
+//console.log(a-b);
+
+//Multiplicação
+//let a = 35;
+//let b = 40;
+//console.log(a*b)
+
+//Divisão
+//let a = 35;
+//let b = 40;
+//console.log(a/b)
+
+//Módulo
+//let a = 35;
+//let b = 40;
+//console.log(a%b)
