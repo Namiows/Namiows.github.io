7ab8d2dcdf0d8bbe071f3a9ca7ac6e248da56676
Kesley Lahoz <namielahoz@gmail.com>
2020-08-11 19:31:09 -0300

    Exercises 1 and 2 done!

---
+++ exercises/7_2/exercise2.js
@@ -0,0 +1,25 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const includingTurnInLesson2 = () => {
+    const turno = "tarde"
+    lesson2.turno = turno
+  };
+  console.log(includingTurnInLesson2());
